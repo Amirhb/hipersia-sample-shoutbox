4b181e0943b610551df67cdf6b2b365f2d3766cc
Amir Hossein Babaeian <amirh.babaeian@gmail.com>
2016-03-08 17:48:26 +0330

    adding /welcome route

--- config/routes.php
+++ config/routes.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 $router = new League\Route\RouteCollection;
 
 $router->addRoute('GET', '/migrate', 'app\controllers\MigrationController::index');
+$router->addRoute('GET', '/welcome', 'app\controllers\DefaultController::welcome');
 $router->addRoute('GET', '/', 'app\controllers\DefaultController::shoutBox');
 $router->addRoute('POST', '/', 'app\controllers\DefaultController::shoutBox');
 $router->addRoute('GET', '/{uri}', 'app\controllers\DefaultController::index');
